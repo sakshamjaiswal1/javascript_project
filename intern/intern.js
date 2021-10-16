350b90831f92e6f4226a92f02772031507b5e0da
Saksham Jaiswal <sjais973@gmail.com>
2021-10-16 22:37:27 +0530

    sass

--- intern/intern.js
+++ intern/intern.js
@@ -1,3 +1,50 @@
 // dcwcfdcdcsdc
-console.log('123')
+// console.log('123')
 // pusing to git with vs code
+// const arr =[1,2,3]
+// const tenetDateVal=['Jan-19',
+//   'Feb-19',
+//   'Mar-19',
+//   'Apr-19',
+//   'May-19',
+//   'Jun-19',
+//   'Jul-19',
+//   'Aug-19',
+//   'Sep-19',
+//   'Oct-19',
+//   'Nov-19',
+//   'Dec-19',
+//   'Jan-20',
+//   'Feb-20',
+//   'Mar-20',
+//   'Apr-20',
+//   'May-20',
+//   'Jun-20',
+//   'Jul-20',
+//   'Aug-20',
+//   'Sep-20',
+//   'Oct-20',
+//   'Nov-20',
+//   'Dec-20',
+//   'Jan-21',
+//   'Feb-21',
+//   'Mar-21',
+//   'Apr-21',
+//   'May-21',
+//   ]
+
+
+//   arr.map((element, tntIdx)=>{
+//         console.log(2*element,tntIdx)
+//      return 2*element  
+//   })
+
+{{}
+   {var a=5}
+
+}
+
+{
+console.log(a)}
+{}
+
